8344af093a92e64ab647aa631ae95af8e1b6e65c
didi <didi@MacBook-Pro-7.local>
2021-04-06 10:25:55 +0800

    让匿名用户可以查看岗位详情

--- jobs/views.py
+++ jobs/views.py
@@ -5,6 +5,7 @@ from jobs.models import Job, Cities, JobTypes
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 
 
 def joblist(request):
@@ -16,3 +17,13 @@ def joblist(request):
         job.job_type = JobTypes[job.job_type][1]
 
     return HttpResponse(template.render(context))
+
+
+def detail(request, job_id):
+    try:
+        job = Job.objects.get(pk=job_id)
+        job.city_name = Cities[job.job_city][1]
+    except Job.DoesNotExist:
+        raise Http404("Job does not exist")
+
+    return render(request, 'job.html', {'job': job})
